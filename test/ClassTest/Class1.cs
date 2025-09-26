d5faa952a062bd7b8f286667a268f7e3ba26aa31
Diego Cando <diego.cando@hsbcad.com>
2025-09-26 14:44:16 -0500

    Same but another repo

---
+++ test/ClassTest/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace ClassTest
+{
+    public class Class1
+    {
+        public void PublicMethod()
+        {
+            PrivateMethod();
+            ProtectedMethod();
+            Console.WriteLine("Public method");
+            Class2 class2 = new Class2();   
+            class2.PublicMethod();
+        }
+        private void PrivateMethod()
+        {
+            string privatev = "private";
+            Console.WriteLine("Private method");
+        }
+        protected void ProtectedMethod()
+        {
+        string protectedv = "protected";
+            Console.WriteLine("Protected method");
+        }
+    }
+
+    class Class2 {
+        public void PublicMethod()
+        {
+            PrivateMethod();
+            ProtectedMethod();
+            Console.WriteLine("Public method");
+        }
+        private void PrivateMethod()
+        {
+            string privatev = "private";
+            Console.WriteLine("Private method");
+        }
+        protected void ProtectedMethod()
+        {
+            string protectedv = "protected";
+            Console.WriteLine("Protected method");
+        }
+    }
+
+}
